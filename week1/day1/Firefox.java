ad6f2157a689edbc4da73c66fba9d9b2bae8e932
Preetha2402 <preethanjali.d@gmail.com>
2024-11-13 22:03:21 +0530

    Add files via upload
    Day1

---
+++ week1/day1/Firefox.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class Firefox {
+
+	
+	public static void main(String[] args) {
+	float browserVersion =100.2f;
+	String browsername ="Firefox";
+	boolean isvisible =true;
+	int releaseYear = 1998;
+	char browserLogo = 'F';
+	
+	System.out.println("The browser name is"+ browsername);
+	System.out.println("version of browser is"+ browserVersion);
+	System.out.println("chech whether the browser is visible:"+ isvisible);
+	System.out.println("Browser released in"+ releaseYear);
+	System.out.println("The logo of browser is"+ browserLogo);
+	
+	
+		
+	
+
+	
+	
+	
+	}
+
+
+}
